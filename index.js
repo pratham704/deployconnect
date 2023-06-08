8a0d1e6730fc618e942f700d52cef9c2e71cf7df
pratham704 <125141594+pratham704@users.noreply.github.com>
2023-06-08 21:25:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const mongoose = require('mongoose');
 const abc = require('./contactmodel');
 const imgsd = require('./newpost');
@@ -7,19 +7,35 @@ const nopostdp = require('./nopostdp');
 const messagemodel = require('./message');
 const storycreate = require('./stories');
 const app = express();
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config();
 const multer = require('multer');
 const { resolve } = require('path');
-const upload = multer({ dest: 'uploads/' })
-app.use('/uploads', express.static('uploads'))
+const upload = multer({ dest: 'uploads/' });
+app.use('/uploads', express.static('uploads'));
 app.use(cors());
 app.use(express.json());
 mongoose.set('strictQuery', false);
 
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
 
-
+const uploadS3 = multer({
+    storage: multerS3({
+        s3,
+        bucket: process.env.S3_BUCKET_NAME,
+        acl: 'public-read',
+        key: function(req, file, cb) {
+            cb(null, `${Date.now().toString()}-${file.originalname}`);
+        }
+    })
+});
 const url = `${process.env.MONGO}`
 
 
@@ -94,7 +110,7 @@ app.post('/createpost', upload.single('image'), async(req, res) => {
 
 
 
-        const imageUrl = req.file.path
+        const imageUrl = req.file.location
 
 
 
@@ -299,7 +315,7 @@ app.post('/dps', upload.single('dp'), async(req, res) => {
 
 
 
-        const check2 = await imgsd.find({ username: req.body.usn }).updateMany({ dp: req.file.path });
+        const check2 = await imgsd.find({ username: req.body.usn }).updateMany({ dp: req.file.location });
         if (check2.upsertedId != null) {
 
             res.json(check2.dp)
@@ -312,7 +328,7 @@ app.post('/dps', upload.single('dp'), async(req, res) => {
 
             if (cd) {
 
-                const check2 = await nopostdp.find({ username: req.body.usn }).updateMany({ dp: req.file.path });
+                const check2 = await nopostdp.find({ username: req.body.usn }).updateMany({ dp: req.file.location });
 
                 const c = await nopostdp.findOne({ username: req.body.usn })
 
@@ -325,7 +341,7 @@ app.post('/dps', upload.single('dp'), async(req, res) => {
 
                 const data = await nopostdp.create({
                     username: req.body.usn,
-                    dp: req.file.path,
+                    dp: req.file.location,
 
                 })
 
@@ -730,9 +746,14 @@ app.post('/postcomment', async(req, res) => {
         const check2 = await imgsd.findOneAndUpdate({ username: posteduser, createdAt: postcreatedat }, { $push: { commentedby: loggedinusn } })
 
 
-        const resp = await imgsd.find({}).sort({ createdAt: -1 })
 
-        const response = resp.map((doc) => {
+        const user1 = await abc.find({ acceptedbyme: loggedinusn })
+        const us = user1.map(u => u.username);
+
+
+        const foundUsers = await imgsd.find({ username: { $in: us } }).sort({ createdAt: -1 }).limit(25);
+
+        const response1 = foundUsers.map((doc) => {
             return {
                 imageUrl: doc.imageUrl,
                 username: doc.username,
@@ -740,14 +761,15 @@ app.post('/postcomment', async(req, res) => {
                 dp: doc.dp,
                 createdAt: doc.createdAt,
                 likes: doc.likes,
-                comments: doc.comments.reverse(),
-                commentedby: doc.commentedby.reverse(),
+                comments: doc.comments,
+                commentedby: doc.commentedby,
 
 
             };
         });
 
-        res.json(response)
+
+        res.json(response1);
 
 
 
@@ -1145,7 +1167,7 @@ app.post('/recentchatsdisplay', async(req, res) => {
 
 
 app.post('/storycreation', upload.single('image'), async(req, res) => {
-    const imageUrl = req.file.path;
+    const imageUrl = req.file.location;
 
 
     const c = await imgsd.findOne({ username: req.body.usn });
