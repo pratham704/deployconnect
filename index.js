4866ca141d508e179367d3263b475fef9ad68ed7
pratham704 <125141594+pratham704@users.noreply.github.com>
2023-06-08 21:36:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const s3 = new AWS.S3({
 const uploadS3 = multer({
     storage: multerS3({
         s3,
-        bucket: process.env.CYC,
+        bucket: process.env.CYCLIC_BUCKET_NAME,
         acl: 'public-read',
         key: function(req, file, cb) {
             cb(null, `${Date.now().toString()}-${file.originalname}`);
